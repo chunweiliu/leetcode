026d3c89087781355e3c97bf52d0086571a19ec3
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-09-05 22:12:57 -0400

    First commit

---
+++ MaximumDepthofBinaryTree.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node
+class TreeNode:
+    def __init__(self, x):
+    	# Preorder
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Tree:
+	def __init__(self):
+		self.root = None
+
+	def addNode(self, data):
+		return TreeNode(data)
+
+	def insert(self, root, data):
+		if root is None:
+			return self.addNode(data)
+		else:
+			if data <= root.val:
+				root.left = self.insert(root.left, data)
+			else:
+				root.right = self.insert(root.right, data)
+			return root
+
+	def printTree(self, root):
+		if root is None:
+			pass
+		else:
+			self.printTree(root.left)
+			print root.val
+			self.printTree(root.right)
+
+
+    # @param root, a tree node
+    # @return an integer
+    # def maxDepth(self, root):
+    #     maxd = 1
+    #     if self.left is None and self.right is None:
+    #     	return maxd
+    #     else:
+    #     	maxDepth(self.left, self)
+    #     	maxDepth(self.right, self)
+        
+if __name__ == "__main__":
+    btree = Tree()
+    root = TreeNode(0)
+    for i in range(5):
+    	data = int(i)+1
+    	btree.insert(root, data)
+
+    btree.printTree(root)
---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode OJ Practice
+
+## Log
+* 07/01/2014 Project start, practiced binary tree, peeked the answer of maxDepth and figured out I was so close to the answer. Feel bad about this.
+* 09/05/2014 Restart the project, aim for an internship!

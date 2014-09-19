534f55fccfe1fbd2a1afb0252d8d0329574888eb
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-09-18 21:22:11 -0400

    AC: special case

---
+++ LengthofLastWord.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def lengthOfLastWord(self, s):
+        # Walk from the end to detect space
+        # Time: O(n)
+        # Space: O(1)
+        sr = s[::-1]
+        count = 0
+        for x in range(len(sr)):
+            if sr[x] == " " and count == 0:  # spacical case
+                pass
+            elif sr[x] == " ":
+                break
+            else:
+                count += 1
+        return count
+
+if __name__ == "__main__":
+    s = "a bbb   "
+    print Solution().lengthOfLastWord(s)

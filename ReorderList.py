74508a75961bf00b696a1b17adf38b0fd61f24bc
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-09-18 22:40:22 -0400

    AC: Space(1)

--- ReorderList.py
+++ ReorderList.py
@@ -9,31 +9,54 @@ class Solution:
     # @param head, a ListNode
     # @return nothing
     def reorderList(self, head):
+        # Reverse the second half and reorder the list in place
+        # Time: O(N)
+        # Space: O(1)
         if head == [] or head is None:
             return
 
-        node_list = []
+        # Find the second half by two pointers trick
+        half = self.findHalf(head)
+
+        # Reverse the second half
+        reversed_half = self.reverse(half)
+
+        # Reorder the list
         curr = head
+        curr_r = reversed_half
+        while curr_r:
+            next, next_r = curr.next, curr_r.next
+            curr.next = curr_r
+            curr_r.next = next
+            curr, curr_r = next, next_r
+        half.next = None
 
-        while curr:
-            node_list.append(curr)
-            curr = curr.next
-
-        curr = node_list.pop(0)
-        first_flag = -1
-        while node_list:
-            if first_flag == 1:
-                curr.next = node_list.pop(0)
+    def findHalf(self, head):
+        fast = head
+        slow = head
+        while head:
+            if fast.next:
+                fast = fast.next
+                if fast.next:
+                    fast = fast.next
             else:
-                curr.next = node_list.pop()
-            curr = curr.next
-            first_flag *= -1
-        curr.next = None
+                break
+            if slow.next:
+                slow = slow.next
+        return slow
+
+    def reverse(self, head):
+        prev, curr, next = None, head, None
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        head = prev
         return head
 
 if __name__ == "__main__":
-    #head = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
-    head = []
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
     head = Solution().reorderList(head)
     while head:
         print head.val

17c708481f04c8c106e338329b2082a85e494b1a
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-10-06 23:59:14 -0400

    AC: conner case: If slow and slow2 was in the same place?

--- LinkedListCycleII.py
+++ LinkedListCycleII.py
@@ -1,22 +1,39 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x, next=None):
-        self.val = x
-        self.next = next
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# [1 -> 2 -> 1]
+#   i  0 1 2 3
+#slow  1 2 1 2
+#fast  1 1 1 1
+#slow2     1 2
 
 
 class Solution:
     # @param head, a ListNode
     # @return a list node
     def detectCycle(self, head):
-        # The intersection is the start of the cycle iff fast and slow start at
-        # the start of the cycle
-        # Time: O(n)
-        # Space: O(1)
+        """The length from head to the start of cycle is k
+        The length from fast and slow meet to the start of cycle is k
+                         +++++++++---
+        Time: O(n)          k  |    |
+        Space: O(1)             \__/
+        """
+        fast = slow = head
+        while fast:
+            fast = fast.next
+            if fast:
+                fast = fast.next
+            slow = slow.next
+            if fast is slow:
+                break
+        if fast is None:
+            return None
 
-
-if __name__ == "__main__":
-    head = ListNode(1, ListNode(2))
-    head.next = head
-    a = Solution().detectCycle(head)
-    print a.val
+        slow2 = head  # slow and slow2 will meet at cycle after k iterations
+        while slow != slow2:
+            slow = slow.next
+            slow2 = slow2.next
+        return slow

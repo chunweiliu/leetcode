05fc21f04511e96d38ada8d72e4afa5b74c22953
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-10-10 16:23:07 -0400

    AC: use only one current node

--- MergeTwoSortedLists.py
+++ MergeTwoSortedLists.py
@@ -2,38 +2,34 @@
 class ListNode:
     def __init__(self, x, next=None):
         self.val = x
-        self.next = next
+        self.next = None
 
 
 class Solution:
     # @param two ListNodes
     # @return a ListNode
     def mergeTwoLists(self, l1, l2):
-        # Make sure change curr.next instead of curr
-        # Time: O(n)
-        # Space: O(1)
+        """Find the next
+        Time: O(n)
+        Space: O(1)
+        Corner: None pointer
+        """
         if l1 is None:
             return l2
         if l2 is None:
             return l1
 
-        head = ListNode(0)
-        curr = head
+        curr = dummy = ListNode(0)
         while l1 and l2:
             if l1.val < l2.val:
-                curr.next = l1  # change curr.NEXT, not curr
+                curr.next = l1
                 l1 = l1.next
             else:
                 curr.next = l2
                 l2 = l2.next
-            curr = curr.next  # curr advance
-
-        if l1:
-            curr.next = l1
-        if l2:
-            curr.next = l2
-        return head.next
-
+            curr = curr.next
+        curr.next = l1 if l1 else l2
+        return dummy.next
 
 if __name__ == "__main__":
     l1 = ListNode(1, ListNode(2, ListNode(2)))

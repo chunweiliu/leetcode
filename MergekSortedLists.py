9a04720f501ac70ce1e1995a5e8490afccee7bfb
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-10-10 16:21:41 -0400

    AC: recursively using merge_two_lists, return ListNode

--- MergekSortedLists.py
+++ MergekSortedLists.py
@@ -9,27 +9,43 @@ class Solution:
     # @param a list of ListNode
     # @return a ListNode
     def mergeKLists(self, lists):
-        import heapq
-        heap = []
-        for x in lists:
-            if x is not None:
-                heap.append((x.val, x))  # tuple for heap key
-        heapq.heapify(heap)
+        """Two solutions
+        1. Use a data structure to maintain k candidates as heap
+        Time: O(n log k)
+        Space: O(k)
+        2. Recursively Merge two lists
+        Time: O(n log k)
+        Space: O(n log k)
+        Corner: length == 0 or 1
+        """
+        if len(lists) == 0:
+            return None
+        if len(lists) == 1:
+            return lists[0]  # return a ListNode
+        if len(lists) == 2:
+            return self.merge_two_list(lists[0], lists[1])
 
-        head = ListNode(0)
-        curr = head
-        while len(heap) > 1:
-            # move the current node
-            min_node = heap[0][1]
-            curr.next = min_node
-            curr = curr.next
-            # update the heap
-            if min_node.next:
-                heapq.heappushpop(heap, (min_node.next.val, min_node.next))
+        head1 = self.mergeKLists(lists[:len(lists)/2])
+        head2 = self.mergeKLists(lists[len(lists)/2:])
+        return self.mergeKLists([head1, head2])
+
+    def merge_two_list(self, head1, head2):
+        if head1 is None:
+            return head2
+        if head2 is None:
+            return head1
+
+        head = tail = ListNode(0)
+        while head1 and head2:
+            if head1.val < head2.val:
+                current = head1
+                head1 = head1.next
             else:
-                heapq.heappop(heap)
-        if heap:
-            curr.next = heap[0][1]
+                current = head2
+                head2 = head2.next
+            tail.next = current
+            tail = tail.next
+        tail.next = head1 or head2
         return head.next
 
 if __name__ == "__main__":

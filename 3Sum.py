71db60c05b825e8b14d4a6461c843967c8bd09c8
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-10-16 22:38:53 -0400

    TLE: Standard two pivots approach.

--- 3Sum.py
+++ 3Sum.py
@@ -1,54 +1,41 @@
 class Solution:
     # @return a list of lists of length 3, [[val1,val2,val3]]
-    MAXNUM = 100
-
     def threeSum(self, num):
-        # Reduce to 2Sum
-        # Time:O(n^2)
-        # Space: O(MAXNUM)
-        if len(num) == 0:
-            return
-
-        min_num = min(num)
-        A = [n - min_num for n in num]
-        A.sort()
+        """Fix a and search b + c = -a
+        Time: O(n^2)
+        Space: O(1)
+        Corner: duplicates in b, c
+        """
+        if not num and len(num) < 2:
+            return []
 
         ans = list()
-        prev_a = None
-        for x in range(len(A)):
-            a = A[x]
-            if a == prev_a:
+        num.sort()
+        for i, a in enumerate(num):
+            # avoid duplicates in a
+            if i > 0 and a == num[i - 1]:
                 continue
 
-            b_and_cs = self.twoSum(A[x+1:], -3*min_num - a)  # why -3*min_num?
-            for bc in b_and_cs:
-                ans.append([a] + bc)
-            prev_a = a
+            j, k = i+1, len(num)-1
+            while j < k:
+                # avoid duplicates in b, c
+                while j > i+1 and num[j] == num[j - 1] and j < k:
+                    j += 1
 
-        # add min_num back
-        for abc in ans:
-            abc[0] += min_num
-            abc[1] += min_num
-            abc[2] += min_num
+                while k < len(num)-1 and num[k] == num[k + 1] and j < k:
+                    k -= 1
+
+                b, c = num[j], num[k]
+                if a + b + c == 0:
+                    ans.append([a, b, c])
+                    j += 1
+                    k -= 1
+                elif a + b + c < 0:
+                    j += 1
+                else:
+                    k -= 1
         return ans
 
-    def twoSum(self, num, target):
-        # find all pairs have sum equal to target
-        # Time: O(n)
-        # Space: O(MAXNUM)
-        A = [0] * self.MAXNUM
-        for n in num:
-            A[n] += 1
-
-        ans = list()
-        for x in range(len(A)):
-            if A[x] > 0:  # Once you count an entry, it has to be eliminated
-                A[x] -= 1
-                if A[target - x] > 0:
-                    A[target - x] -= 1
-                    ans.append([x, target - x])
-
-        return ans
 
 if __name__ == "__main__":
     num = [-1, 0, 1, 2, -1, -4]

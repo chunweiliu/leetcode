84775267a37ff741f2b617b8990f9b92359c1065
Chun-Wei Liu <chunwei@cs.unc.edu>
2014-10-19 22:20:40 -0400

    AC: Don't forget set None for large

--- PartitionList.py
+++ PartitionList.py
@@ -10,47 +10,23 @@ class Solution:
     # @param x, an integer
     # @return a ListNode
     def partition(self, head, x):
-        if head is None:
-            return head
+        """Partition list into 3 parts and relink them
+        Time: O(n)
+        Space: O(1)
+        """
+        if not head:
+            return None
 
-        les_tail = ListNode(0)  # key
-        lrg_tail = ListNode(0)
-
-        # store the heads of linked lists (no difference)
-        les_head = les_tail
-        lrg_head = lrg_tail
-
-        while head is not None:
+        small = small_head = ListNode(0)
+        large = large_head = ListNode(0)
+        while head:
             if head.val < x:
-                les_tail.next = ListNode(head.val)  # key
-                les_tail = les_tail.next
+                small.next = head
+                small = small.next
             else:
-                lrg_tail.next = ListNode(head.val)
-                lrg_tail = lrg_tail.next
+                large.next = head
+                large = large.next
             head = head.next
-        lrg_tail.next = None  # key
-        les_tail.next = lrg_head.next
-        return les_head.next
-
-    def print_list(self, head):
-        if head.next is not None:
-            print head.val
-            self.print_list(head.next)
-        else:
-            print head.val
-
-
-if __name__ == "__main__":
-    # a = ListNode(1)
-    # a.next = ListNode(4)
-    # a.next.next = ListNode(3)
-    # a.next.next.next = ListNode(2)
-    # a.next.next.next.next = ListNode(5)
-    # a.next.next.next.next.next = ListNode(2)
-    a = ListNode(2)
-    a.next = ListNode(1)
-    x = 2
-    Solution().print_list(a)
-    print "+++"
-    b = Solution().partition(a, x)
-    Solution().print_list(b)
+        large.next = None  # not always true, need to set it up
+        small.next = large_head.next
+        return small_head.next

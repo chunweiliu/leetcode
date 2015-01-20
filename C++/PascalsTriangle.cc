a0748e5af1047040d3a962c986a19d6b82342a99
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-01-20 14:00:48 -0500

    Updated format of codes

--- C++/PascalsTriangle.cc
+++ C++/PascalsTriangle.cc
@@ -3,37 +3,43 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-	vector<vector<int> > generate(int num_rows) {
-		vector<vector<int> > pascals_triangle;
-		if (num_rows == 0) return pascals_triangle;
+    vector<vector<int> > generate(int num_rows)
+    {
+        vector<vector<int> > pascals_triangle;
+        if (num_rows == 0) return pascals_triangle;
 
-		pascals_triangle.push_back(vector<int>(1, 1));  // first row
+        pascals_triangle.push_back(vector<int>(1, 1));  // first row
 
-		for (int i = 1; i < num_rows; ++i) {  // build from the second
-			const int num_columns = i+1;
-			vector<int> current(num_columns, 1);
-			const vector<int> &previous = pascals_triangle[i-1];
+        for (int i = 1; i < num_rows; ++i)    // build from the second
+        {
+            const int num_columns = i + 1;
+            vector<int> current(num_columns, 1);
+            const vector<int> &previous = pascals_triangle[i - 1];
 
-			for (int j = 1; j < num_columns-1; ++j) {  // build the middle
-				current[j] = previous[j-1] + previous[j];
-			}
-			pascals_triangle.push_back(current);
-		}
-		return pascals_triangle;
-	}
+            for (int j = 1; j < num_columns - 1; ++j)  // build the middle
+            {
+                current[j] = previous[j - 1] + previous[j];
+            }
+            pascals_triangle.push_back(current);
+        }
+        return pascals_triangle;
+    }
 };
 
 int main()
 {
-	const int num_rows = 5;
-	Solution pascals_triangle;
-	const vector<vector<int> > &solution = pascals_triangle.generate(num_rows);
-	for (int i = 0; i < num_rows; ++i) {
-		for (int j = 0; j <= i; ++j) {
-			cout << solution[i][j] << " ";
-		}
-		cout << endl;
-	}
+    const int num_rows = 5;
+    Solution pascals_triangle;
+    const vector<vector<int> > &solution = pascals_triangle.generate(num_rows);
+    for (int i = 0; i < num_rows; ++i)
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            cout << solution[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
--- C++/ReverseInteger.cc
+++ C++/ReverseInteger.cc
@@ -1,24 +1,27 @@
 #include <iostream>
 
-class ReverseInteger {
+class ReverseInteger
+{
 public:
-	int reverse(int x) {
-		double r = 0;  // float is not enough
-		int s = x > 0 ? 1 : -1;
-		for (x = s * x; x; x /= 10) {
-			r = r * 10 + x % 10;
-		}
-		if (s*r >= std::numeric_limits<int>::min() &&
-			s*r <= std::numeric_limits<int>::max())
-			return (int) s*r;
-		else return 0;
-	}
+    int reverse(int x)
+    {
+        double r = 0;  // float is not enough
+        int s = x > 0 ? 1 : -1;
+        for (x = s * x; x; x /= 10)
+        {
+            r = r * 10 + x % 10;
+        }
+        if (s * r >= std::numeric_limits<int>::min() &&
+                s * r <= std::numeric_limits<int>::max())
+            return (int) s * r;
+        else return 0;
+    }
 };
 
 int main()
 {
-	int x = 15342364;
-	// int x = 2234;
-	ReverseInteger reverse_integer;
-	std::cout << reverse_integer.reverse(x) << std::endl;
+    int x = 15342364;
+    // int x = 2234;
+    ReverseInteger reverse_integer;
+    std::cout << reverse_integer.reverse(x) << std::endl;
 }

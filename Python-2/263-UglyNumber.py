c3d3d2c71e28fabd8d082906a9ac8f1526d384f1
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-09-23 22:52:47 -0400

    Update code.

---
+++ Python-2/263-UglyNumber.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        # If a number has any prime factor other than 2, 3, 5, it's a ugly one.
+        prime_factors = [2, 3, 5]
+        for prime_factor in prime_factors:
+            while num % prime_factor == 0:
+                num //= prime_factor
+        return True if num == 1 else False
+
+if __name__ == '__main__':
+    num = 0
+    print Solution().isUgly(num)

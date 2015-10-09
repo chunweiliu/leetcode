55a3e379e1efa0e07bdfacbe36dad39f6913d92f
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-10-08 20:49:58 -0700

    Update code.

--- Python-2/034-SearchForARange.py
+++ Python-2/034-SearchForARange.py
@@ -5,32 +5,31 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # Search for the left interval.
-        left_interval = -1
+        return [self.left_search(nums, target),
+                self.right_search(nums, target)]
+
+    def left_search(self, nums, target):
+        left_interval = -1  # [BUG] left_interval = 0
         i, j = 0, len(nums) - 1
         while i <= j:
             m = i + (j - i) / 2
-            if nums[m] >= target:
-                if nums[m] == target:
-                    left_interval = m
+            if target <= nums[m]:
                 j = m - 1
+                if target == nums[m]:  # [BUG] N/A
+                    left_interval = m
             else:
                 i = m + 1
+        return left_interval
 
-        # Search for the right interval.
+    def right_search(self, nums, target):
         right_interval = -1
         i, j = 0, len(nums) - 1
         while i <= j:
             m = i + (j - i) / 2
-            if nums[m] <= target:
-                if nums[m] == target:
-                    right_interval = m
+            if target >= nums[m]:
                 i = m + 1
+                if target == nums[m]:
+                    right_interval = m
             else:
                 j = m - 1
-        return [left_interval, right_interval]
-
-if __name__ == '__main__':
-    nums = [1]
-    target = 0
-    print Solution().searchRange(nums, target)
+        return right_interval

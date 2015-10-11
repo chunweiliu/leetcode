0c7efc4b48cf00b7ee2988a191e7641ba148cd4c
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-10-10 20:56:21 -0700

    Update code.

--- Python-2/025-ReverseNodesInKGroup.py
+++ Python-2/025-ReverseNodesInKGroup.py
@@ -12,11 +12,10 @@ class Solution(object):
         :type k: int
         :rtype: ListNode
         """
-        # Check if the list need to be reversed.
         if not head or k <= 1:
             return head
 
-        # Check if the list is long enough to be reversed.
+        # Check if the list is long enough to reverse
         curr, n = head, 0
         while curr and n < k:
             curr = curr.next
@@ -24,34 +23,24 @@ class Solution(object):
         if n < k:
             return head
 
-        # Reverse the first k nodes (k and the lenght of list are at least 2)
-        # ^---------v
-        # 0    1 <- 2    3
-        #      v---------^
-        # prev last curr
+        # Set the label
         dummy = ListNode(0)
         dummy.next = head
+
+        # ^-----------v
+        # 0     1<----2      3
+        #       v------------^
+        # prev, last, curr
         prev = dummy
         last = prev.next
         curr = last.next
 
-        n = 0
-        while last and n < k - 1:  # k - 1, two nodes only need one reverse.
+        # Reverse k - 1 pairs
+        for _ in range(k - 1):
             last.next = curr.next
             curr.next = prev.next
             prev.next = curr
             curr = last.next
-            n += 1
 
-        # Recursively do for the next group.
         last.next = self.reverseKGroup(last.next, k)
         return dummy.next
-
-if __name__ == '__main__':
-    # head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
-    head = ListNode(1, ListNode(2, ListNode(3)))
-    k = 3
-    reversed_head = Solution().reverseKGroup(head, k)
-    while reversed_head:
-        print reversed_head.val
-        reversed_head = reversed_head.next

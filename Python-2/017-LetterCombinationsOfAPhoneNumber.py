92ef1e7aa5b5a5165596c22acb51363ca37b67a5
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-10-10 22:22:44 -0700

    Update code.

--- Python-2/017-LetterCombinationsOfAPhoneNumber.py
+++ Python-2/017-LetterCombinationsOfAPhoneNumber.py
@@ -10,21 +10,25 @@ class Solution(object):
         # Recursively append the letters
         # Time: O(n!)
         # Space: O(n)
-        if not digits:
-            return list()
-        elif len(digits) == 1:
-            return list(self.buttons[digits[0]])
-        else:
-            combinations = self.letterCombinations(digits[1:])
-            # List comprehension
-            # combinations_ = list()
-            # for letter in self.buttons[digits[0]]:
-            #     for post in combinations:
-            #         combinations_.append(letter + post)
-            # return combinations_
-            return [letter + post
-                    for letter in self.buttons[digits[0]]  # Outer loop
-                    for post in combinations]  # Inner loop
+        if len(digits) == 1:
+            try:
+                return [char for char in self.buttons[digits[0]]]
+            except KeyError:
+                return []
+
+        combinations = self.letterCombinations(digits[1:])
+        try:
+            ans = []
+            for char in self.buttons[digits[0]]:
+                for combination in combinations:
+                    ans.append(char + combination)
+            return ans
+        except KeyError:
+            return combinations
+
+        # return [letter + post
+        #         for letter in self.buttons[digits[0]]  # Outer loop
+        #         for post in combinations]  # Inner loop
 
 if __name__ == "__main__":
-    print Solution().letterCombinations("234")
+    print Solution().letterCombinations("1234")
